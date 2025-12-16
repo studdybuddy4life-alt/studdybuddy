db8506985e7a7a738dcdb3584b70d1d170a231b0
studdybuddy4life-alt <studdybuddy4life@gmail.com>
2025-12-16 18:07:31 +0530

    Update _app.js

--- Frontend/pages/_app.js
+++ Frontend/pages/_app.js
@@ -1,2 +1,9 @@
-import '../styles/globals.css'
-export default function MyApp({ Component, pageProps }){ return <Component {...pageProps} /> }
+import { ClerkProvider } from "@clerk/nextjs";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ClerkProvider>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+}
