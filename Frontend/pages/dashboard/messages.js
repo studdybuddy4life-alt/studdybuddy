e2fdc0ce1654ee74d0e73c53769bd6f0b5eef51c
studdybuddy4life-alt <studdybuddy4life@gmail.com>
2025-12-17 11:22:54 +0530

    Create messages.js

---
+++ Frontend/pages/dashboard/messages.js
@@ -0,0 +1,8 @@
+export default function Messages() {
+  return (
+    <div style={{ padding: "2rem", color: "white" }}>
+      <h1>💬 Messages</h1>
+      <p>Tutor chat system will be added here.</p>
+    </div>
+  );
+}
